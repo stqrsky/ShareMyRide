13b4f59838cb2544d26af7a32c0f6459fa3c1367
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-20 14:58:05 +0100

    add Api Route for LoginController

---
+++ src/backend/laravel/app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function submit(Request $request)
+    {
+        // perform login - psuedo code:
+        // validate the phone number
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
+
+        // find or create a user model
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!user) {
+            return response()->json(['message' => 'Could not process a user with that phone number.'], 401);
+        }
+
+        // send the user a one-time use code
+        $user->notify();
+
+        // return back a response
+
+    }
+}
