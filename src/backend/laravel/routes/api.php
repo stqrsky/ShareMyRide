ef080282b207bd0c4aa870a31e307ed1c44d2f66
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-20 16:40:14 +0100

    add driver routes and function -> show/update

--- src/backend/laravel/routes/api.php
+++ src/backend/laravel/routes/api.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
 
-Route::get('/user', function(Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
+
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+});
