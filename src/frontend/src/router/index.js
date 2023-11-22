23195213e77020c92ac5c825fd879a5426009318
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-22 12:53:56 +0100

    passengers FindARide-functions

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import LandingView from '@/views/LandingView.vue'
+import LocationView from '@/views/LocationView.vue'
+import MapView from '@/views/MapView.vue'
+import TripView from '@/views/TripView.vue'
+
 
 import axios from 'axios'
 
@@ -16,6 +20,21 @@ const router = createRouter({
       path: '/landing',
       name: 'landing',
       component: LandingView
+    },
+    {
+      path: '/location',
+      name: 'location',
+      component: LocationView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    },
+    {
+      path: '/trip',
+      name: 'trip',
+      component: TripView
     }
   ]
 })
--- src/frontend/src/views/LandingView.vue
+++ src/frontend/src/views/LandingView.vue
@@ -9,6 +9,7 @@
                         Start Driving
                     </button>
                     <button
+                        @click="handleFindARide"
                         class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
                         Find A Ride
                     </button>
@@ -18,5 +19,14 @@
     </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleFindARide = () => {
+    router.push({
+        name: 'location'
+    })
+}
 
 </script>
