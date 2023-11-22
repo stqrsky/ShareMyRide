86ff5b6fdb5a6a3e6a667eaeea1389b122d61908
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-22 12:14:07 +0100

    handleLogin + handleVerification

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -2,17 +2,32 @@
 <template>
        <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">Bitte geben Sie Ihre Telefonnummer ein.</h1>
-        <form action="#">
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input type="text" v-maska data-maska="(####)###-#######" name="phone" id="phone" placeholder="(+49) 1753445678"
+                        <input type="text" v-maska data-maska="# (###)###-#######" v-model="credentials.phone" name="phone" id="phone" placeholder="(+49) 1753445678"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button type="submit"
+                    <button type="submit" @submit.prevent="handleLogin"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
+                </div>
+            </div>
+        </form>
+
+        <form v-else action="#" @submit.prevent="handleVerification">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="phone" id="phone" placeholder="(+49) 1753445678"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" @submit.prevent="handleVerification"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Verify</button>
                 </div>
             </div>
         </form>
@@ -20,7 +35,44 @@
 </template>
 
 <script setup>
-    import { vMaska } from "maska"
+    import { vMaska } from 'maska'
+    import { reactive } from 'vue'
+    import axios from 'axios'
+
+    const credentials = reactive({
+        phone: null
+    })
+
+    const waitingOnVerification = ref(false)
+
+    const handleLogin = () => {
+        axios.post('http://localhost/api/login', {
+            phone: credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '').replace('-', ''),
+        })
+            .then((response) => {
+                console.log(response.data)
+                waitingOnVerification.value = true;
+            })
+            .catch((error) => {
+                console.error(error)
+                alert(error.response.data.message)
+            })
+    }
+
+    const handleVerification = () => {
+    axios.post('http://localhost/api/login/verify', {
+        phone: credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '').replace('-', ''),
+        login_code: credentials.login_code
+    })
+        .then((response) => {
+            console.log(response.data) // auth token
+            localStorage.setItem('token, response.data')
+        })
+        .catch((error) => {
+            console.error(error)
+            alert(error.response.data.message)
+        })
+}
 </script>
 
 <style>
