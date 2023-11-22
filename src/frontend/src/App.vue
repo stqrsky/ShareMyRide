d36648cd5beb8116c57474387ec20dc2a47b005e
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-22 10:35:08 +0100

    npm maska -> frontend

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -3,6 +3,8 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-
-  <RouterView />
+  
+  <div class="min-h-screen bg-gray-100 text-center antialiased" >
+    <RouterView />
+  </div>
 </template>
